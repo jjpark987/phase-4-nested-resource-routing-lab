586b9a4a6511a30ed7e95033a3fdb3a911171bdb
Jason Park <jjpark987@gmail.com>
2023-07-04 09:31:16 -0700

    commit tree c0e892b9bbd46914e61307fa1ba9fa3d03c2689e

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,47 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_message
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_message
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else 
+      items = Item.all
+    end
     render json: items, include: :user
   end
 
+  def show
+    item = Item.find(params[:id])
+    render json: item, include: :user
+  end
+
+  # def create
+  #   user = User.find(params[:user_id])
+  #   item = Item.create!(item_params)
+  #   render json: item, include: :user, status: :created
+  # end
+  def create
+    user = User.find(params[:user_id])
+    if user
+      item = Item.create!(item_params.merge(user: user))
+      render json: item, include: :user, status: :created
+    end
+  end
+  
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_not_found_message e
+    render json: { error: e }, status: :not_found
+  end
+
+  def render_invalid_message e 
+    render json: { error: e.record.errors.full_messages }, status: :unprocessable_entity
+  end
 end
